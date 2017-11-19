659ee75f619051a625b902647a5756df657c1083
pkukharenka <pyotr1989@gmail.com>
2017-11-19 19:53:57 +0300

    1.1. Создание Hello World[#31341]

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+/**
+* It's a program that prints an expression "Hello world." to the console.
+*@author Pyotr Kukharenka
+*/
+
+public class Calculate {
+	
+	public static void main (String[] args) {
+		System.out.println("Hello world.");
+	}
+}
+
