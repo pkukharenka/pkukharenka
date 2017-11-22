bb5494f22f8f0573cb0ce0109e9e94d050b4e69d
pkukharenka <pyotr1989@gmail.com>
2017-11-22 21:52:40 +0300

    4.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,31 @@
+package ru.job4j.loop;
+
+/**
+ * Class for counting factorial n.
+ *
+ * @author Pyotr Kukharenka
+ * @since 22.11.2017
+ */
+public class Factorial {
+
+    /**
+     * Factorial n.
+     *
+     * @param n - number.
+     * @return factorial n or exception.
+     */
+    public int calc(final int n) {
+        int result = 1;
+        if (n > 1) {
+            for (int i = 1; i <= n; i++) {
+                result = result * i;
+            }
+            return result;
+        } else if (n == 0) {
+            result = 1;
+        } else {
+            throw new ArithmeticException("Attempt to calculate the factorial of a negative number");
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Test for factorial task.
+ *
+ * @author Pyotr Kukharenka
+ * @since 22.11.2017
+ */
+public class FactorialTest {
+
+    /**
+     * Test factorial with normal number.
+     */
+    @Test
+    public void whenFactorial5Then120() {
+        Factorial fact = new Factorial();
+        final int result = fact.calc(5);
+        assertThat(result, is(120));
+    }
+
+    /**
+     * Test factorial with negative number.
+     */
+    @Test(expected = ArithmeticException.class)
+    public void whenNegativeNumberThenException() {
+        Factorial fact = new Factorial();
+        final int result = fact.calc(-5);
+    }
+
+    /**
+     * Test factorial with zero.
+     */
+    @Test
+    public void whenZeroThenOne() {
+        Factorial fact = new Factorial();
+        final int result = fact.calc(0);
+        assertThat(result, is(1));
+    }
+
+}
