4df1313d64bdeb49e11ee2e7b1fe438955e8bd59
pkukharenka <pyotr1989@gmail.com>
2017-11-22 21:21:14 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for cycle tasks.
+ *
+ * @author Pyotr Kukharenka
+ * @since 22.11.2017
+ */
+
+package ru.job4j.loop;
---
+++ chapter_001/src/test/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Tests for cycle tasks.
+ *
+ * @author Pyotr Kukharenka
+ * @since 22.11.2017
+ */
+
+package ru.job4j.loop;
