3e00aaf9ab7d9887fe843be8c1e395c4337416e3
pkukharenka <pyotr1989@gmail.com>
2018-01-03 11:57:47 +0300

    2. Сделать ожидание вывода. [#1017]

--- chapter_005/src/main/java/ru/job4j/thread/Counter.java
+++ chapter_005/src/main/java/ru/job4j/thread/Counter.java
@@ -9,29 +9,62 @@ package ru.job4j.thread;
  */
 
 public class Counter {
+    /**
+     * Текст для обработки.
+     */
+    private final String text;
 
-    public static void main(String[] args) {
-        String value = "Thank you for submitting your request.";
-        char[] chars = value.toCharArray();
+    /**
+     * Конструктор для инициализации текста.
+     *
+     * @param text - текст для обработки.
+     */
+    public Counter(String text) {
+        this.text = text;
+    }
 
-        //Поток для подсчета количества пробелов в строке.
-        new Thread() {
+    /**
+     * Возвращает текст ввиде массива символов.
+     *
+     * @param text - текст для обработки.
+     * @return - текст ввиде массива символов.
+     */
+    private char[] toChar(final String text) {
+        return this.text.toCharArray();
+
+    }
+
+    /**
+     * Возвращает поток, подсчитывающий количество пробелов в тексте.
+     *
+     * @return - поток, подсчитывающий количество пробелов в тексте.
+     */
+    private Thread calcSpace() {
+        Thread space = new Thread(new Runnable() {
             @Override
             public void run() {
                 int count = 0;
-                for (char c : chars) {
+                for (char c : toChar(text)) {
                     if (c == 32) {
                         count++;
                     }
                 }
                 System.out.println(String.format("Количество пробелов: %s", count));
             }
-        }.start();
+        });
+        return space;
+    }
 
-        //Поток для подсчета количества слов в строке.
-        new Thread() {
+    /**
+     * Возвращает поток, подсчитывающий количество слов в тексте.
+     *
+     * @return - поток, подсчитывающий количество слов в тексте.
+     */
+    private Thread calcWord() {
+        Thread word = new Thread(new Runnable() {
             @Override
             public void run() {
+                char[] chars = toChar(text);
                 int count = 0;
                 for (int i = 1; i < chars.length; i++) {
                     if ((chars[i] < 65 || (chars[i] > 90 && chars[i] < 97) || chars[i] > 122) &&
@@ -41,7 +74,28 @@ public class Counter {
                 }
                 System.out.println(String.format("Количество слов: %s", count));
             }
-        }.start();
+        });
+        return word;
+    }
 
+    /**
+     * Запуск программы.
+     *
+     * @param args - входящие аргументы
+     */
+    public static void main(String[] args) {
+        Counter counter = new Counter("Thank you for submitting your request.");
+        System.out.println("Start");
+        Thread tr1 = counter.calcSpace();
+        Thread tr2 = counter.calcWord();
+        tr1.start();
+        tr2.start();
+        try {
+            tr1.join();
+            tr2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finish");
     }
 }
