c12c75a1ece39758863cf5b54ac8ac964ae4253a
pkukharenka <pyotr1989@gmail.com>
2018-05-18 13:36:27 +0300

    Travis

--- car_platform/src/main/resources/import.sql
+++ car_platform/src/main/resources/import.sql
@@ -1,9 +1,3 @@
---импорт справочных данных о брэндах и моделях авто.
-COPY model FROM 'E:/model.csv' DELIMITER ',' CSV;
-
---импорт опций и характеристик автомобилей.
-COPY option FROM 'E:/option.csv' DELIMITER ',' CSV ENCODING 'windows-1251';
-
 INSERT INTO roles (id, role_type) VALUES (1, 'Администратор');
 
 INSERT INTO users (id, email, user_name, password, role_id)  VALUES (1, 'admin', 'admin', '123', 1);
